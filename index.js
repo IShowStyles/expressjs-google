02f00ba324d01ccbab836d445190f6bf6a159131
zholudev111 <zholudev111@gmail.com>
2024-02-28 21:14:55 +0200

    added fix

--- index.js
+++ index.js
@@ -1,62 +1,52 @@
 const express = require('express');
-const {OAuth2Client,google} = require('google-auth-library');
-const {PrismaClient} = require('@prisma/client');
+const {OAuth2Client} = require('google-auth-library');
+const open = require('open');
 
-const prisma = new PrismaClient();
 const app = express();
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = 'https://expressjs-google.onrender.com/auth/google/callback';
+async function getAuthenticatedClient() {
+  return new Promise((resolve, reject) => {
+    const oAuth2Client = new OAuth2Client(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      `https://expressjs-google.onrender.com/auth/google/callback`
+    );
 
-const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/userinfo.profile',
+    });
 
-app.get('/auth/google', (req, res) => {
-  const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/userinfo.email',
-    ],
+    open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+
+    app.get('/auth/google/callback', async (req, res) => {
+      try {
+        const { code } = req.query;
+        console.log(`Code is ${code}`);
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        console.info('Tokens acquired.');
+        res.send('Authentication successful! Please return to the console.');
+        resolve(oAuth2Client);
+      } catch (e) {
+        reject(e);
+        res.status(500).send('Authentication failed');
+      }
+    });
   });
-  res.redirect(url);
-});
+}
 
-app.get('/auth/google/callback', async (req, res) => {
-  const {code} = req.query;
-  console.log(code);
-  const {tokens} = await oauth2Client.getToken(code);
-  console.log();
-  oauth2Client.setCredentials(tokens);
-  const oauth2 = google.get()
-  console.log(oauth2);
-  // const userInfo = a/wait oauth2.get()
-  // console.log(oauth2);
-  // const user/ = await prisma.user.upsert({
-  //   where: {googleId: userInfo.data.id},
-  //   update: {
-  //     accessToken: tokens.access_token,
-  //     refreshToken: tokens.refresh_token,
-  //   },
-  //   create: {
-  //     googleId: userInfo.data.id,
-  //     email: userInfo.data.email,
-  //     name: userInfo.data.name,
-  //     accessToken: tokens.access_token,
-  //     refreshToken: tokens.refresh_token,
-  //   },
-  // });
+async function main() {
+  const oAuth2Client = await getAuthenticatedClient();
+  const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
+  const res = await oAuth2Client.request({url});
+  console.log(res.data);
 
-  res.send('Authentication successful!');
-});
+  const tokenInfo = await oAuth2Client.getTokenInfo(oAuth2Client.credentials.access_token);
+  console.log(tokenInfo);
+}
 
-const PORT = process.env.PORT;
-
-app.get('/', (req, res)=> {
-  res.send("helloworld")
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`);
+  main().catch(console.error);
 });
